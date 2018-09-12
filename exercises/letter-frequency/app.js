68808e0f8ec203dd6674eb48ad590ab54ead3d19
Brooke <Brooke.horrocks7@gmail.com>
2018-09-12 09:56:17 -0600

    letter-frequency update

--- exercises/letter-frequency/app.js
+++ exercises/letter-frequency/app.js
@@ -1,17 +1,38 @@
 var phrase = "slimy smelly solution";
 
+//My solution
 function arrayIndex(str){
     var charCounterObj = {
         noDupesString: ""
     };
     for(var i = 0; i < str.length; i++){
-        if(charCounterObj[str[i]] === undefined){
+        if(charCounterObj[str[i]]){ //charCounterObj[str[i]] is a truthy value
+            charCounterObj[str[i]]++;
+        } else {
             charCounterObj[str[i]] = 1;
             charCounterObj.noDupesString += str[i];
-        } else {
-            charCounterObj[str[i]]++;
         }
     }
+    return charCounterObj;
+}
+
+//Instructor solution demonstrating for in loop to loop through object
+function arrayIndex(str){
+    var charCounterObj = {};
+    for(var i = 0; i < str.length; i++){
+        if(charCounterObj[str[i]]){ //charCounterObj[str[i]] is a truthy value
+            charCounterObj[str[i]]++;
+        } else {
+            charCounterObj[str[i]] = 1;
+            charCounterObj.noDupesString += str[i];
+        }
+    }
+
+    var noDupesString = "";
+    for(var key in charCounterObj){
+        noDupesString += key;
+    }
+
     return charCounterObj;
 }
 
