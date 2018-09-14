ab2b81bda5b622e042dfbda098d35630f58c8121
Brooke <Brooke.horrocks7@gmail.com>
2018-09-14 10:57:26 -0600

    dj-js update

--- exercises/dj-js/app.js
+++ exercises/dj-js/app.js
@@ -1,40 +1,40 @@
 var mySquare = document.getElementById("square");
 var myBody = document.getElementById("main");
 
-mySquare.addEventListener('mouseover', function(){
+mySquare.addEventListener('mouseover', function() {
     mySquare.style.backgroundColor = "blue";
-    mySquare.addEventListener("mouseleave", function(){
+    mySquare.addEventListener("mouseleave", function() {
         mySquare.style.backgroundColor = "black";
     })
 })
 
-mySquare.addEventListener("mousedown", function(){
+mySquare.addEventListener("mousedown", function() {
     mySquare.style.backgroundColor = "red";
-    mySquare.addEventListener("mouseup", function(){
+    mySquare.addEventListener("mouseup", function() {
         mySquare.style.backgroundColor = "yellow";
     })
 })
 
-mySquare.addEventListener("dblclick", function(){
+mySquare.addEventListener("dblclick", function() {
     mySquare.style.backgroundColor = "green";
 })
 
-myBody.addEventListener("wheel", function(){
+myBody.addEventListener("wheel", function() {
     mySquare.style.backgroundColor = "orange"
 })
 
-myBody.addEventListener("keypress", function(e){
+myBody.addEventListener("keypress", function(e) {
     if(e.keyCode === 114) {
         mySquare.style.backgroundColor = "red";
     } else if(e.which === 112) {
-        mySquare.style.backgroundColor= "purple"
+        mySquare.style.backgroundColor = "purple"
     } else if(e.which === 121) {
-        mySquare.style.backgroundColor= "yellow"
+        mySquare.style.backgroundColor = "yellow"
     } else if(e.which === 103) {
-        mySquare.style.backgroundColor= "green"
+        mySquare.style.backgroundColor = "green"
     } else if(e.which === 98) {
-        mySquare.style.backgroundColor= "blue"
+        mySquare.style.backgroundColor = "blue"
     } else if(e.which === 111) {
-        mySquare.style.backgroundColor= "orange"    
+        mySquare.style.backgroundColor = "orange"    
     }
 })  
