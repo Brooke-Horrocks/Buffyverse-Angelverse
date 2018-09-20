786be75ede56e17e041ddd09444d16b77bacf5e6
Brooke <Brooke.horrocks7@gmail.com>
2018-09-20 10:33:53 -0600

    missing-no initial

---
+++ exercises/missing-no/app.js
@@ -0,0 +1,12 @@
+function findMissingNo(arr){
+    arr.sort(function(a, b){
+        return a - b;
+    });
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] !== arr[i + 1] - 1){
+            return arr[i + 1] - 1;
+        }
+    }
+}
+
+module.exports = findMissingNo;
