29ed609113ca5afd263d543135b8cea77223285d
Brooke <Brooke.horrocks7@gmail.com>
2018-10-25 15:17:34 -0600

    socket.io tutorial/intro

---
+++ exercises/socket-timer/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import { subscribeToTimer } from "./api";
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      timestamp: "no timestamp yet"
+    }
+    subscribeToTimer((err, timestamp) => this.setState({
+      timestamp
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>
+          This is the timer value: {this.state.timestamp}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
